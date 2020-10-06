501bc42ae41593b52664e96ddafdabe9dd40d60b
matteo.adduci <matteo.adduci@wink.by>
2020-10-06 16:23:17 +0200

    aggiunti componenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BaseLayoutComponent } from './containers/base-layout/base-layout.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoDetailComponent } from './components/todo-detail/todo-detail.component';
+import { TodoListItemComponent } from './components/todo-list-item/todo-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseLayoutComponent,
+    TodoListComponent,
+    TodoDetailComponent,
+    TodoListItemComponent
   ],
   imports: [
     BrowserModule
