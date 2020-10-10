805847d85ba74f6ad2c273c86275d44c3bd270fb
matteo.adduci <matteo.adduci@wink.by>
2020-10-10 13:06:54 +0200

    restructured modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BaseLayoutComponent } from './containers/base-layout/base-layout.component';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { TodoDetailComponent } from './components/todo-detail/todo-detail.component';
-import { TodoListItemComponent } from './components/todo-list-item/todo-list-item.component';
+import { TodoListComponent } from './modules/todo/components/todo-list/todo-list.component';
+import { TodoDetailComponent } from './modules/todo/components/todo-detail/todo-detail.component';
+import { TodoListItemComponent } from './modules/todo/components/todo-list-item/todo-list-item.component';
 
 @NgModule({
   declarations: [
