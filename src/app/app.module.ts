d22496dd25664269c8e2164cf4d44afd2f6c3f53
matteo.adduci <matteo.adduci@wink.by>
2020-10-13 11:28:50 +0200

    rotte configurate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BaseLayoutComponent } from './containers/base-layout/base-layout.component';
 import { TodoModule } from './modules/todo/todo.module';
+import { Route, RouterModule } from '@angular/router';
+
+const routes: Route[] = [
+  {
+    path: 'todos',
+    loadChildren: () => import('./modules/todo/todo.module').then(m => m.TodoModule)
+  }
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +20,8 @@ import { TodoModule } from './modules/todo/todo.module';
   ],
   imports: [
     BrowserModule,
-    TodoModule
+    TodoModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -3,6 +3,22 @@ import { CommonModule } from '@angular/common';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoDetailComponent } from './components/todo-detail/todo-detail.component';
 import { TodoListItemComponent } from './components/todo-list-item/todo-list-item.component';
+import { Route, RouterModule } from '@angular/router';
+
+const routes: Route[] = [
+  {
+    path: "todos/list",
+    component: TodoListComponent
+  },
+  {
+    path: "todos/detail",
+    component: TodoDetailComponent
+  },
+  {
+    path: "todos/list-item",
+    component: TodoListItemComponent
+  },
+];
 
 
 
@@ -16,7 +32,8 @@ import { TodoListItemComponent } from './components/todo-list-item/todo-list-ite
     TodoListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class TodoModule { }
