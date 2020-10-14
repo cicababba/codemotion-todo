b774e9d1297028fb4c84158faff7bc43d6187f23
matteo.adduci <matteo.adduci@wink.by>
2020-10-14 10:18:22 +0200

    aggiornato app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { BaseLayoutComponent } from './containers/base-layout/base-layout.component';
 import { TodoModule } from './modules/todo/todo.module';
 import { Route, RouterModule } from '@angular/router';
+import { SharedModule } from './modules/shared/shared.module';
+
 
 const routes: Route[] = [
   {
@@ -21,6 +23,7 @@ const routes: Route[] = [
   imports: [
     BrowserModule,
     TodoModule,
+    SharedModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
